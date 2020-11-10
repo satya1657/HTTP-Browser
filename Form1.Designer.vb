d75bd009616f5ede91ee4b63dd58dfd2a17593bc
satyaprakash <kumarsatyaprakash33@gmail.com>
2020-11-10 15:14:35 +0530

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.txbAddress = New System.Windows.Forms.TextBox()
+        Me.txbSearch = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tmrStatus = New System.Windows.Forms.Timer(Me.components)
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 70)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(984, 422)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'txbAddress
+        '
+        Me.txbAddress.Location = New System.Drawing.Point(264, 28)
+        Me.txbAddress.Name = "txbAddress"
+        Me.txbAddress.Size = New System.Drawing.Size(326, 20)
+        Me.txbAddress.TabIndex = 1
+        '
+        'txbSearch
+        '
+        Me.txbSearch.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txbSearch.Location = New System.Drawing.Point(706, 29)
+        Me.txbSearch.Name = "txbSearch"
+        Me.txbSearch.Size = New System.Drawing.Size(162, 20)
+        Me.txbSearch.TabIndex = 2
+        Me.txbSearch.Text = "Search"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(61, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Back"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(79, 27)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(62, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Next"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(596, 25)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(54, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "GO"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button4.Location = New System.Drawing.Point(874, 26)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(48, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "GO"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button5.Location = New System.Drawing.Point(928, 26)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(44, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "..."
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(198, 26)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(53, 23)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "Refresh"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 501)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 9
+        '
+        'tmrStatus
+        '
+        Me.tmrStatus.Enabled = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(147, 26)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(45, 23)
+        Me.Button7.TabIndex = 10
+        Me.Button7.Text = "Home"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(984, 561)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txbSearch)
+        Me.Controls.Add(Me.txbAddress)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(1000, 600)
+        Me.Name = "Form1"
+        Me.Text = "ENM Browser"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents txbAddress As TextBox
+    Friend WithEvents txbSearch As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tmrStatus As Timer
+    Friend WithEvents Button7 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.ScriptErrorsSuppressed = True
+    End Sub
+    Sub checkAdress() Handles WebBrowser1.DocumentCompleted
+        txbAddress.Text = WebBrowser1.Url.ToString
+    End Sub
+
+    Private Sub tmrStatus_Tick(sender As Object, e As EventArgs) Handles tmrStatus.Tick
+        Label1.Text = WebBrowser1.StatusText.ToString
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If WebBrowser1.CanGoBack Then
+            WebBrowser1.GoBack()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If WebBrowser1.CanGoForward Then
+            WebBrowser1.GoForward()
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        WebBrowser1.Refresh()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        WebBrowser1.Navigate(txbAddress.Text)
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If My.Settings.SE = "Google" Then
+            WebBrowser1.Navigate(My.Settings.Google + txbSearch.Text)
+        ElseIf My.Settings.SE = "Bing" Then
+            WebBrowser1.Navigate(My.Settings.Bing + txbSearch.Text)
+        End If
+    End Sub
+
+    Private Sub txbSearch_TextChanged(sender As Object, e As EventArgs) Handles txbSearch.Click
+        If txbSearch.Text = "Search" Then
+            txbSearch.Text = ""
+
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Form2.Show()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        WebBrowser1.Navigate("www.google.com")
+    End Sub
+End Class
